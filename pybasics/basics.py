651ba6c05a32a4ca012fea8c2434416fa1d2a4d7
Riteshm-07 <147985146+Riteshm-07@users.noreply.github.com>
2026-05-19 08:07:34 +0000

    #

---
+++ pybasics/basics.py
@@ -0,0 +1,15 @@
+# Simple Python Program
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello,", name)
+print("You are", age, "years old.")
+
+# Simple calculation
+num1 = 10
+num2 = 5
+
+sum = num1 + num2
+
+print("Sum =", sum)
